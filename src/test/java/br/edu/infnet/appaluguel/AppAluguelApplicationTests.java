15b235350fb9a1b9478e8b90cc881da7c1289f03
Eduardo De Lucca <eduardo.luccati@gmail.com>
2022-11-27 15:16:47 -0300

    Configuraçcoes iniciais App-Aluguel

---
+++ src/test/java/br/edu/infnet/appaluguel/AppAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appaluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
